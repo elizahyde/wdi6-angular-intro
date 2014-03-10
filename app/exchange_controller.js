c12a34359d36aeb12226b95e804801fef17d0467
elizahyde <elizabeth@elizahyde.com>
2014-03-10 13:38:27 -0700

    my updated changes

--- app/exchange_controller.js
+++ app/exchange_controller.js
@@ -1,8 +1,11 @@
 
 wdi.ExchangeController = function($scope) {
-  $scope.convert = function() {
+  $scope.convert = function( euros ) {
 
     $scope.dollars = $scope.euros * 1.35;
   };
 
+  $scope.$watch('euros', function(newValue, oldValue){
+    $scope.isNumber = !isNaN(newValue);
+  });
 };
